0697eb76239aea49acc6249e7236d3b02bfdacf4
OctaEDLP00 <octavio.pincha@gmail.com>
2023-05-02 02:46:41 -0300

    change: create const file and rewrite parseCountry

---
+++ src/utils/const.js
@@ -0,0 +1,145 @@
+/**
+ * @property ARGENTINA
+ * @property BOLIVIA
+ * @property BRASIL
+ * @property CHILE
+ * @property COLOMBIA
+ * @property COSTA_RICA
+ * @property CUBA
+ * @property ECUADOR
+ * @property EL_SALVADOR
+ * @property ESPANA
+ * @property GUATEMALA
+ * @property HAITI
+ * @property HUNDURAS
+ * @property MEXICO
+ * @property NICARAGUA
+ * @property PANAMA
+ * @property PARAGUAY
+ * @property PERU
+ * @property REPUBLICA_DOMINICANA
+ * @property URUGUAY
+ * @property VENEZUELA
+ */
+export const COUNTRIES_DICTIONARY = {
+  ARGENTINA: 'arg',
+  BOLIVIA: 'bol',
+  BRASIL: 'bra',
+  CHILE: 'chl',
+  COLOMBIA: 'col',
+  COSTA_RICA: 'cri',
+  CUBA: 'cub',
+  ECUADOR: 'ecu',
+  EL_SALVADOR: 'slv',
+  ESPANA: 'es',
+  GUATEMALA: 'gtm',
+  HAITI: 'hti',
+  HONDURAS: 'hnd',
+  MEXICO: 'mex',
+  NICARAGUA: 'nic',
+  PANAMA: 'pan',
+  PARAGUAY: 'pry',
+  PERU: 'per',
+  REPUBLICA_DOMINICANA: 'dom',
+  URUGUAY: 'ury',
+  VENEZUELA: 'ven'
+}
+
+/**
+ * @property ARGENTINA
+ * @property BOLIVIA
+ * @property BRASIL
+ * @property CHILE
+ * @property COLOMBIA
+ * @property COSTA_RICA
+ * @property CUBA
+ * @property ECUADOR
+ * @property EL_SALVADOR
+ * @property ESPANA
+ * @property GUATEMALA
+ * @property HAITI
+ * @property HUNDURAS
+ * @property MEXICO
+ * @property NICARAGUA
+ * @property PANAMA
+ * @property PARAGUAY
+ * @property PERU
+ * @property REPUBLICA_DOMINICANA
+ * @property URUGUAY
+ * @property VENEZUELA
+ */
+export const COUNTRY_PARAM = {
+  ARGENTINA: 'ARGENTINA',
+  BOLIVIA: 'BOLIVIA',
+  BRASIL: 'BRASIL',
+  CHILE: 'CHILE',
+  COLOMBIA: 'COLOMBIA',
+  COSTA_RICA: 'COSTA_RICA',
+  CUBA: 'CUBA',
+  ECUADOR: 'ECUADOR',
+  EL_SALVADOR: 'EL_SALVADOR',
+  ESPANA: 'ESPANA',
+  GUATEMALA: 'GUATEMALA',
+  HAITI: 'HAITI',
+  HONDURAS: 'HONDURAS',
+  MEXICO: 'MEXICO',
+  NICARAGUA: 'NICARAGUA',
+  PANAMA: 'PANAMA',
+  PARAGUAY: 'PARAGUAY',
+  PERU: 'PERU',
+  REPUBLICA_DOMINICANA: 'REPUBLICA_DOMINICANA',
+  URUGUAY: 'URUGUAY',
+  VENEZUELA: 'VENEZUELA'
+}
+
+
+// with ts
+/*
+export enum COUNTRIES_DICTIONARY {
+  ARGENTINA = 'arg',
+  BOLIVIA = 'bol',
+  BRASIL = 'bra',
+  CHILE = 'chl',
+  COLOMBIA = 'col',
+  COSTA_RICA = 'cri',
+  CUBA = 'cub',
+  ECUADOR = 'ecu',
+  EL_SALVADOR = 'slv',
+  ESPANA = 'es',
+  GUATEMALA = 'gtm',
+  HAITI = 'hti',
+  HONDURAS = 'hnd',
+  MEXICO = 'mex',
+  NICARAGUA = 'nic',
+  PANAMA = 'pan',
+  PARAGUAY = 'pry',
+  PERU = 'per',
+  REPUBLICA_DOMINICANA = 'dom',
+  URUGUAY = 'ury',
+  VENEZUELA = 'ven'
+}
+
+export enum COUNTRY_PARAM {
+  ARGENTINA = 'ARGENTINA',
+  BOLIVIA = 'BOLIVIA',
+  BRASIL = 'BRASIL',
+  CHILE = 'CHILE',
+  COLOMBIA = 'COLOMBIA',
+  COSTA_RICA = 'COSTA_RICA',
+  CUBA = 'CUBA',
+  ECUADOR = 'ECUADOR',
+  EL_SALVADOR = 'EL_SALVADOR',
+  ESPANA = 'ESPANA',
+  GUATEMALA = 'GUATEMALA',
+  HAITI = 'HAITI',
+  HONDURAS = 'HONDURAS',
+  MEXICO = 'MEXICO',
+  NICARAGUA = 'NICARAGUA',
+  PANAMA = 'PANAMA',
+  PARAGUAY = 'PARAGUAY',
+  PERU = 'PERU',
+  REPUBLICA_DOMINICANA = 'REPUBLICA_DOMINICANA',
+  URUGUAY = 'URUGUAY',
+  VENEZUELA = 'VENEZUELA'
+}
+*/
