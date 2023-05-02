95bc57617555c82bdd7d1404293988e4dc0b1985
OctaEDLP00 <octavio.pincha@gmail.com>
2023-05-01 21:49:16 -0300

    change(endopoint): name in func getEndpointFetch

--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -1,8 +1,8 @@
-import { getEndpointFecth } from '../utils/getEndpointFetch.js'
+import { getEndpointFetch } from '../utils/getEndpointFetch.js'
 import { parseCountry } from '../utils/parseCountry.js'
 
-export const CountryData = async country => await getEndpointFecth(`https://sueldos.dev/api/get-country-data?country=${parseCountry(country)}`)
+export const CountryData = async country => await getEndpointFetch(`https://sueldos.dev/api/get-country-data?country=${parseCountry(country)}`)
 
-export const FilteredSalary = async country => await getEndpointFecth(`https://sueldos.dev/api/filtered-salary?country=${parseCountry(country)}`)
+export const FilteredSalary = async country => await getEndpointFetch(`https://sueldos.dev/api/filtered-salary?country=${parseCountry(country)}`)
 
 // console.log({ countryData, filteredSalary })
